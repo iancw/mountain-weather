64680e9b92e3a74cdefe25c12e8ed23dce4cf225
Ian Will <icwill@gmail.com>
2013-01-21 22:39:44 -0500

    Finished building the basic map site

--- ice/conditions/views.py
+++ ice/conditions/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render
+
+from conditions.models import Location
+
+def index(request):
+	locations = Location.objects.all()
+	context = {'locations': locations}
+	return render(request, 'index.html', context)
--- ice/ice/urls.py
+++ ice/ice/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ice.views.home', name='home'),
-    # url(r'^ice/', include('ice.foo.urls')),
+    url(r'^ice/', include('conditions.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,3 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
