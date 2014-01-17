aeb4799f9befa386b2d13ef44a306e519aa43c62
Ian Will <ian.will@nrl.navy.mil>
2014-01-16 20:43:26 -0500

    Removed old code, heroku-ized stuff

--- dep-django/conditions/templatetags/ice_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-def to_fahrenheit(k):
-	return ((k - 273.15) * 1.8) + 32.0
-
-register.filter('to_fahrenheit', to_fahrenheit)
--- dep-django/conditions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from conditions import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^location/(?P<location_id>\d+)/$', views.detail, name='detail'),
-)
